966afc6e40c967256fd30f82c692523406308494
PassamaquoddyPhil <hiimjron@gmail.com>
2025-09-11 09:40:28 -0400

    Add files via upload

---
+++ cusum.m
@@ -0,0 +1,12 @@
+start = 0;
+finish = 0;
+data = [10,9,8,7,6,5,4,3,2,1];
+arraybian = [];
+mucus(arraybian, start, finish);
+
+function mucus = mucus(data, arraybian, start, fininsh)
+    aver = mean(data);
+    return;
+
+end
+
