e73f993217f25fe99ce7f87fc8fb3cdcc4aa3ce9
PassamaquoddyPhil <hiimjron@gmail.com>
2025-09-11 10:32:10 -0400

    Add files via upload

--- cusum.m
+++ cusum.m
@@ -1,11 +1,23 @@
+fprintf("Type the file name:");
+fileName = input('', "s");
 start = 0;
 finish = 0;
-data = [10,9,8,7,6,5,4,3,2,1];
+data = parse_datafile(fileName);
 arraybian = [];
 mucus(arraybian, start, finish);
 
-function mucus = mucus(data, arraybian, start, fininsh)
+function mucus(data, arraybian, start, finish)
     aver = mean(data);
+    cusumData = 0;
+    for(i = start:finish)
+        cusumData(i+1) = cusumData(i) + (data(i) - aver);
+    end
+    maxi = max(abs(cusumData));
+    if(bootstrap(data) > 0.95)
+        mucus(data, arraybian, start, index(max(abs(cusumData))));
+        mucus(data, arraybian, index(max(abs(cusumData))), finish);
+        arraybian = index(abs(max(cusumData)));
+    end
     return;
 
 end
