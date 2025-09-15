a98e549ff3d1a4f33d1786b708c750659758dc32
PassamaquoddyPhil <hiimjron@gmail.com>
2025-09-15 11:00:24 -0400

    Add files via upload

---
+++ bootstrap.m
@@ -0,0 +1,4 @@
+function a = bootstrap(data, OGDiff)
+
+
+end
--- cusum.m
+++ cusum.m
@@ -1,21 +1,29 @@
 fprintf("Type the file name:");
 fileName = input('', "s");
+fprintf("What is your desired confidence interval?");
+confidence = input('');
+fprintf("What is the number of bootstraps?");
+bootstraps = input('');
 data = parseCSV(fileName);
-sizie = size(data);
+%sizie = size(data);
 arraybian = [];
-mucus(data, arraybian, sizie);
+mucus(data, arraybian, sizie, confidence, 1);
 
-function mucus(data, arraybian, sizie)
+function [arraybian] = mucus(data, arraybian, confidence, iteration)
+    hold on;
+    plot(data);
     aver = mean(data);
     cusumData = 0;
+    sizie = size(data);
     for(i = sizie(1):sizie(2))
         cusumData(i+1) = cusumData(i) + (data(i) - aver);
     end
+    plot(cusumData);
     OGDiff = max(cusumData) - min(cusumData);
-    if(bootstrap(data, OGDiff) > 0.95)
-        mucus(data, arraybian, sizie(1), index(max(abs(cusumData))));
-        mucus(data, arraybian, index(max(abs(cusumData))), sizie(2));
-        arraybian = index(abs(max(cusumData)));
+    if(bootstrap(data, OGDiff) > confidence)
+        mucus(data(sizie(1)):data(index(max(abs(cusumData)))), arraybian, confidence, size(arraybian)+1);
+        mucus(data(index(max(abs(cusumData)))):data(sizie(2)), arraybian, confidence, size(arraybian)+2);
+        arraybian(iteration) = index(abs(max(cusumData)));
     end
     return;
 
