ddc2f77db05b5d3bbeef9a4a769269a50b1711b5
PassamaquoddyPhil <hiimjron@gmail.com>
2025-09-19 22:49:45 -0400

    Add files via upload

--- cusum.m
+++ cusum.m
@@ -32,25 +32,57 @@ for i = 1:length(changepoints)
 end
 
 %% MUCUS FUNCTION
-% 
+% Takes in an array of numbers, a desired confidence level, a number of
+% times to perform bootstrapping, and the index of the beginning of the
+% data.
+% Returns the indicies of where a change in the mean was detected in the
+% original array of numbers
 function changepoints = mucus(data, confidence, bootstraps, start)
+    % Create an array to hold the results
     changepoints = [];
-    aver = mean(data);
-    sizie = length(data);
-    cusumData = zeros(1, sizie + 1);
-    for i = 1:sizie
-        cusumData(i+1) = cusumData(i) + (data(i) - aver);
+    % Find the average
+    data_avg = mean(data);
+    % Calculate the lenght of the array
+    length_data = length(data);
+    % Create an array to hold the cusum value points
+    cusumData = zeros(1, length_data + 1);
+    
+    % Calculate the cusum from the data. 
+    for i = 1:length_data
+        cusumData(i+1) = cusumData(i) + (data(i) - data_avg);
     end
     
+    % Calculate the difference between the maximum of the cusum and the
+    % minimum of the cusum
     OGDiff = max(cusumData) - min(cusumData);
-    if(bootstrap(data, OGDiff, aver, bootstraps) > confidence)
-        x_vals = start + (1:sizie + 1);
+
+    % Bootstrap the data to determine if it is statistically significant.
+    % If so, run the code to record the data point and further break up the
+    % data.
+    if(bootstrap(data, OGDiff, data_avg, bootstraps) > confidence)
+        % Generate an array of consecutive integers starting at the index
+        % of where the array or subarray begins.
+        x_vals = start + (1:length_data + 1);
+        % Plot the cusum on the graph of the data, using x_vals for the
+        % x-values and cusumData for the y-values
         plot(x_vals, cusumData, '*r');
-        [~, idx] = max(abs(cusumData));
-        cp = idx - 1;
+        
+        % Determine the index of the point furthest from 0 in the cusum
+        [~, index] = max(abs(cusumData));
+        % Set the change point at 1 below the index since it would have
+        % moved forward 1 because of the cusum
+        cp = index - 1;
+
+        % Perform a cusum recursively on the data on the left of the
+        % changepoint and then recursively on the data on the right of the
+        % changepoint
         left = mucus(data(1:cp), confidence, bootstraps, start);
         right = mucus(data(cp:end), confidence, bootstraps, cp);
-        right = right + cp;
+        % Correct the right changepoint by shifting it over from 0 by the
+        % changepoint. Subtract 1 for addition correction
+        right = right + cp - 1;
+
+        % Capture the results of the cusum analysis in an array.
         changepoints = [left, cp, right];
     end
     return;
